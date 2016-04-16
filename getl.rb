c727813f0ba20ecbbb3d5bb1ef1973a96ac32b16
srydos <srydos@gmail.com>
2016-04-16 22:34:23 +0900

    secret keys removed

---
+++ getl.rb
@@ -0,0 +1,17 @@
+require 'twitter'
+require 'pp'
+client = Twitter::REST::Client.new do |config|
+  file = File.open(".twitter_keys","r")
+    lines = file.readlines
+    config.consumer_key = lines[0].chomp
+    config.consumer_secret = lines[1].chomp
+    config.access_token = lines[2].chomp
+    config.access_token_secret  = lines[3].chomp
+  file.close
+end
+client.home_timeline.each do |tweet|
+  puts tweet.full_text
+  puts #{tweet.friends_timeline}
+# puts "FAVORITE: #{tweet.favorite_count}"
+# puts "RETWEET : #{tweet.retweet_count}"
+end
---
+++ twi.rb
@@ -0,0 +1,17 @@
+require 'twitter'
+require 'pp'
+client = Twitter::REST::Client.new do | config |
+  file = File.open(".twitter_keys","r")
+    lines = file.readlines
+    config.consumer_key	= lines[0].chomp
+    config.consumer_secret = lines[1].chomp
+    config.access_token	= lines[2].chomp
+    config.access_token_secret	= lines[3].chomp
+  file.close
+end
+msg=ARGV[0] 
+if msg==nil then
+  print "input massage! : "
+  msg=STDIN.gets
+end
+client.update(msg)
