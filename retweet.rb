7203de36ae6fee98c0d63149ac2e5316c6e12d1f
srydos <srydos@gmail.com>
2017-03-30 15:05:42 +0900

    adapt retweet.rb

--- retweet.rb
+++ retweet.rb
@@ -1,18 +1,10 @@
 #!/usr/bin/env ruby
 WORK_DIR=File.expand_path(__FILE__).sub(/[^\/]+$/,'')
-require 'twitter'
-require 'pp'
-require 'yaml'
-key = YAML.load_file(WORK_DIR + "/user.yml")
-client = Twitter::REST::Client.new(
-  consumer_key:        key["consumer_key"],
-  consumer_secret:     key["consumer_secret"],
-  access_token:        key["access_token"],
-  access_token_secret: key["access_token_secret"]
-)
+require WORK_DIR + 'TetesolTwitter.rb'
+retweet_user = TetesolTwitter.new
 if ARGV[0] == nil or ARGV[0] == '' then
   print "arg : (retweet_target_id)"
   exit
 end
 retweet_id = ARGV[0]
-client.retweet(retweet_id)
+retweet_user.retweet(retweet_id)
