7ac7c9a0ea420ff909413b088900b859bfff9b82
srydos <srydos@gmail.com>
2017-05-16 00:55:50 +0900

    -> bot

--- bot.rb
+++ bot.rb
@@ -4,7 +4,7 @@ require WORK_DIR + 'Class/TetesolTwitter.rb'
 require WORK_DIR + 'Class/TetesolStreaming.rb'
 #require WORK_DIR + 'reply.rb'
 #require WORK_DIR + 'get_tl.rb'
-unko_client   = TetesolTwitter.new(WORK_DIR + 'Config/unko_client.yml')
+rest_client   = TetesolTwitter.new(WORK_DIR + 'Config/unko_client.yml')
 stream_client = TetesolStreaming.new(WORK_DIR + 'Config/stream.yml')
 last_reply_id = "1"
 
@@ -14,10 +14,14 @@ func_name = ""
 func_name = "debug_mode" if args.delete("-d") or args.delete("-debug")
 
 #最後に反応したtweet_idを取得
-last_reply_id = unko_client.read_or_make_text_file(WORK_DIR + "Config/.last_reply_id")
+last_reply_id = rest_client.read_or_make_text_file(WORK_DIR + "Config/.last_reply_id")
+
+#反応する条件を読み込み
+@condition = YAML.load_file('Config/reaction_condition.yml')
+@condition['reaction']
 
 #mention取得
-monitored_tl = unko_client.mentions_timeline_bot(last_reply_id)
+monitored_tl = rest_client.mentions_timeline_bot(last_reply_id)
 monitored_tl.reverse.each do |tweet|
   @text = tweet.full_text
   if @text.match("うんこ") or @text.match("クソ") then
@@ -25,10 +29,10 @@ monitored_tl.reverse.each do |tweet|
     #内容重複よけ　それでも被ったら流さないでいい
     rand(15).times{ @str += "…"}
     #なぜ人は排便時に水を流すのか
-    unko_client.reply(tweet.id ,"ジャーッ" + @str).id unless func_name == "debug_mode"
+    rest_client.reply(tweet.id ,"ジャーッ" + @str).id unless func_name == "debug_mode"
     last_reply_id = tweet.id
     pp last_reply_id if func_name == "debug_mode"
   end
 end
-#今回反応した最後のtweet_idを保存
-unko_client.write_text_to_file(WORK_DIR + "Config/.last_reply_id", last_reply_id)
+#最後のtweet_idを保存
+rest_client.write_text_to_file(WORK_DIR + "Config/.last_reply_id", last_reply_id)
