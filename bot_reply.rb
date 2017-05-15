cddc569de888779cd327cfbcd0eae13f50b88f9e
srydos <srydos@gmail.com>
2017-05-15 15:07:28 +0900

    debug

--- bot_reply.rb
+++ bot_reply.rb
@@ -1,12 +1,23 @@
 #!/usr/bin/env ruby
 WORK_DIR=File.expand_path(__FILE__).sub(/[^\/]+$/,'')
 require WORK_DIR + 'Class/TetesolTwitter.rb'
+require WORK_DIR + 'Class/TetesolStreaming.rb'
 #require WORK_DIR + 'reply.rb'
 #require WORK_DIR + 'get_tl.rb'
-tweet_user   = TetesolTwitter.new(WORK_DIR + 'Config/user.yml')
-last_reply_id = 1
-read_or_make_text_file(WORK_DIR + "Config/.last_reply_id")
-monitored_tl = tweet_user.mentions_timeline_bot(last_reply_id.to_i)
+unko_client   = TetesolTwitter.new(WORK_DIR + 'Config/unko_client.yml')
+stream_client = TetesolStreaming.new(WORK_DIR + 'Config/stream.yml')
+last_reply_id = "1"
+
+#使用機能判定
+args = ARGV
+func_name = ""
+func_name = "debug_mode" if args.delete("-d") or args.delete("-debug")
+
+#最後に反応したtweet_idを取得
+last_reply_id = unko_client.read_or_make_text_file(WORK_DIR + "Config/.last_reply_id")
+
+#mention取得
+monitored_tl = unko_client.mentions_timeline_bot(last_reply_id)
 monitored_tl.reverse.each do |tweet|
   @text = tweet.full_text
   if @text.match("うんこ") or @text.match("クソ") then
@@ -14,8 +25,10 @@ monitored_tl.reverse.each do |tweet|
     #内容重複よけ　それでも被ったら流さないでいい
     rand(15).times{ @str += "…"}
     #なぜ人は排便時に水を流すのか
-    tweet_user.reply(tweet.id ,"ジャーッ" + @str).id
+    unko_client.reply(tweet.id ,"ジャーッ" + @str).id unless func_name == "debug_mode"
     last_reply_id = tweet.id
+    pp last_reply_id if func_name == "debug_mode"
   end
 end
-write_text_to_file(WORK_DIR + "Config/.last_reply_id", last_reply_id)
+#今回反応した最後のtweet_idを保存
+unko_client.write_text_to_file(WORK_DIR + "Config/.last_reply_id", last_reply_id)
