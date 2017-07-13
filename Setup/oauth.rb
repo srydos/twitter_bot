754f4c2da88f76ff2fbde1a6fb45ebf78ca6475a
srydos <srydos@gmail.com>
2017-07-13 12:22:26 +0900

    fixed and ope check about serorin merge.

--- Setup/oauth.rb
+++ Setup/oauth.rb
@@ -1,8 +1,9 @@
 require 'oauth'
 require 'oauth/consumer'
+require 'fileutils'
 require 'yaml'
 
-if (ARGV.length != 2) then
+if (ARGV.length < 2) then
   puts "require args (consumer_key) (consumer_secret_key)\'"
   exit
 end
@@ -11,30 +12,31 @@ consumer_key = ARGV[0]
 consumer_secret = ARGV[1]
 yaml_name = ARGV[2] || "user"
 
-consumer = OAuth::Consumer.new(consumer_key, @consumer_secret, {site: "https://api.twitter.com"})
+consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {site: "https://api.twitter.com"})
 request_token = consumer.get_request_token
 
 puts "get pin in this url"
 puts  request_token.authorize_url
 #urlにアクセスしてから
+sleep 5
 print "input PIN : "
 pin = STDIN.gets
 
 access_token = request_token.get_access_token(:oauth_verifier => pin)
 
-key_hash = { 
-  consumer_key:		consumer_key,
-  consumer_secret:	consumer_secret,
-  access_token:		access_token.token,
-  access_token_secret:	access_token.secret
+key_hash = {
+  consumer_key:       consumer_key,
+  consumer_secret:    consumer_secret,
+  access_token:       access_token.token,
+  access_token_secret:access_token.secret
 }
 
 # Configディレクトリ準備
-current_path = File.expand_path(File.dirname(__FILE__) + '../')
-FileUtil.mkdir_p("#{current_path}/Config") if File.exist?(current_path)
+config_path = File.expand_path(__FILE__).sub(/[^\/]+$/,'') + "../Config"
+FileUtils.mkdir_p(config_path) unless File.exist?(config_path)
 
-open("#{current_path}/Config/#{yaml_name}.yml","w+") do |yml|
+open("#{config_path}/#{yaml_name}.yml","w+") do |yml|
   YAML.dump(key_hash, yml)
 end
 
-puts "#{current_path}/Config/#{yaml_name}.yml"
+puts "#{File.expand_path(config_path)}/#{yaml_name}.yml"
